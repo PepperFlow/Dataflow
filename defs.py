9691f252e435e85a7814ff5df02cd81894c1ad40
Wuttichai Chueaklang <pete@MacBook-Pro-som-tillhor-Wuttichai.local>
2025-10-30 21:05:29 +0100

    Uppdate

--- defs.py
+++ defs.py
@@ -1,16 +1,15 @@
 from dagster import job, op, Definitions
 import subprocess
-import os
 
 @op(name="extract_and_load_op")
 def extract_and_load():
-    print("--- Startar DLT-pipelinen (load_job_ads_dlt.py) ---")
+    print("--- Startar DLT-pipelinen ---")
     subprocess.run(["python", "load_job_ads_dlt.py"], check=True)
     return True
 
 @op(name="transform_data_op")
 def transform_data(start_signal): 
-    print("--- Startar dbt-transformation (dbt run) ---")
+    print("--- Startar dbt-transformation ---")
     subprocess.run(
         ["dbt", "run", "--project-dir", "job_ads_dbt"], 
         check=True
@@ -21,7 +20,7 @@ def transform_data(start_signal):
 def job_ads_etl_pipeline():
     transform_data(start_signal=extract_and_load())
 
+# VIKTIGT: Detta är vad Dagster letar efter!
 defs = Definitions(
     jobs=[job_ads_etl_pipeline]
 )
-# dagster dev -f defs.py
