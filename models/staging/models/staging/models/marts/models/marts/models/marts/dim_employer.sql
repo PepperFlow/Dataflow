4ad7259c08900784fb682c172f8abf87f6726b96
saidabdi97 <said.abdi9708@gmail.com>
2025-11-27 20:34:04 +0100

    Initial clean version of Big Data and Cloud project

--- models/staging/models/staging/models/marts/models/marts/models/marts/dim_employer.sql
+++
@@ -1,6 +0,0 @@
--- Dimension: Arbetsgivare
-SELECT DISTINCT
-    {{ dbt_utils.generate_surrogate_key(['employer_name']) }} as employer_key,
-    employer_name
-FROM {{ ref('stg_job_ads') }}
-WHERE employer_name IS NOT NULL
